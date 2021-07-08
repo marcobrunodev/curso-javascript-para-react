45925b58a07e60d092602f88a5773bc1e73ff01d
Marco Bruno <marco.bruno.br@gmail.com>
2021-07-09 00:47:08 +0200

    :sparkles: Finish the BoardGame object

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import "./src/styles/elements/base.css";
 import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(4);
+const $htmlBoardGame = BoardGame(6);
 
 $root.insertAdjacentHTML("beforeend", $htmlBoardGame);
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,12 +1,15 @@
+import "./style.css";
 import CardGame from "../../components/CardGame";
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame();
-  const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+  const $htmlContent = $htmlCardGame.repeat(amountCards);
 
-  console.log($htmlBoardGame);
-
-  return $htmlBoardGame;
+  return `
+    <section class="board-game">
+      ${$htmlContent}
+    </section>
+  `;
 }
 
 export default BoardGame;
