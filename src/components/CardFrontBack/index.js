e694042ed2e6ce2e62a0bd9a8e6644587fd78f19
Marco Bruno <marco.bruno.br@gmail.com>
2021-07-14 00:39:39 +0200

    :sparkles: Add the CardGame with javascript icon

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,7 +4,7 @@ function CardFrontBack() {
   return /*html*/ `
     <article class="card-front-back">
       ${CardGame()}
-      ${CardGame()}
+      ${CardGame("javascript", "Logo do JavaScript")}
     </article>
   `;
 }
